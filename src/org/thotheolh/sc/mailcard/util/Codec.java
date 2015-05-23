2234f3c16c5f4205ccf572e1fe62972c33244d8d
ASKGLab <research@askg.info>
2015-05-23 10:52:42 +0800

    Corrects the Codec to handle '0' digit integers at the start of OTP numbers as '0' is a valid OTP digit. Example OTP, 55667, implicitly omits the '0' but this must be included into the OTP code for authentication.

--- src/org/thotheolh/sc/mailcard/util/Codec.java
+++ src/org/thotheolh/sc/mailcard/util/Codec.java
@@ -17,6 +17,11 @@ public class Codec {
         byte[] intAsciiBytes = new byte[]{0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
             0x36, 0x37, 0x38, 0x39};
 
+        // Convert 5 digit results to 6 digit for OTP use case as 0 is still valid
+        if (input <= 99999) {
+            result[0] = intAsciiBytes[0];
+        }
+        
         while (input > 0) {
             result[result.length - ctr] = intAsciiBytes[input % 10];
             input = input / 10;
